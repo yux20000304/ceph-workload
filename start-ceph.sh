a315c709ffe89608b7f74171678fcb2943732570
yux20000304 <2056404413@qq.com>
2022-03-07 19:34:44 +0800

    add ls

--- start-ceph.sh
+++ start-ceph.sh
@@ -10,31 +10,40 @@ cd ${dir}
 ./bin/ceph-fuse -c ./ceph.conf /mnt/cephfs
 
 #执行工作负载
-filebench -f /home/yyx/project/workload/ceph-workload.f
+filebench -f /home/yyx/project/ceph-workload/workload/create-workload.f
+
+
 
 #转存osd信息
-./bin/ceph daemon osd.0 perf dump > /home/yyx/project/workload/result/osd0.json
+./bin/ceph daemon osd.0 perf dump > /home/yyx/project/ceph-workload/result/osd0.json
 
-./bin/ceph daemon osd.1 perf dump > /home/yyx/project/workload/result/osd1.json
+./bin/ceph daemon osd.1 perf dump > /home/yyx/project/ceph-workload/result/osd1.json
 
-./bin/ceph daemon osd.2 perf dump > /home/yyx/project/workload/result/osd2.json
+./bin/ceph daemon osd.2 perf dump > /home/yyx/project/ceph-workload/result/osd2.json
 
 #转存mon信息
-./bin/ceph daemon mon.a perf dump > /home/yyx/project/workload/result/mona.json
+./bin/ceph daemon mon.a perf dump > /home/yyx/project/ceph-workload/result/mona.json
 
-./bin/ceph daemon mon.b perf dump > /home/yyx/project/workload/result/monb.json
+./bin/ceph daemon mon.b perf dump > /home/yyx/project/ceph-workload/result/monb.json
 
-./bin/ceph daemon mon.c perf dump > /home/yyx/project/workload/result/monc.json
+./bin/ceph daemon mon.c perf dump > /home/yyx/project/ceph-workload/result/monc.json
 
 #转存mgr信息
-./bin/ceph daemon mgr.x perf dump > /home/yyx/project/workload/result/mgrx.json
+./bin/ceph daemon mgr.x perf dump > /home/yyx/project/ceph-workload/result/mgrx.json
 
 #转存mds信息
-./bin/ceph daemon mds.a perf dump > /home/yyx/project/workload/result/mdsa.json
+./bin/ceph daemon mds.a perf dump > /home/yyx/project/ceph-workload/result/mdsa.json
 
-./bin/ceph daemon mds.b perf dump > /home/yyx/project/workload/result/mdsb.json
+./bin/ceph daemon mds.b perf dump > /home/yyx/project/ceph-workload/result/mdsb.json
 
-./bin/ceph daemon mds.c perf dump > /home/yyx/project/workload/result/mdsc.json
+./bin/ceph daemon mds.c perf dump > /home/yyx/project/ceph-workload/result/mdsc.json
+
+./bin/ceph daemon mds.a perf schema > /home/yyx/project/ceph-workload/result/mdsa_schema.json
+
+./bin/ceph daemon mds.b perf schema > /home/yyx/project/ceph-workload/result/mdsb_schema.json
+
+./bin/ceph daemon mds.c perf schema > /home/yyx/project/ceph-workload/result/mdsc_schema.json
+
 
 #关闭集群
 ../src/stop.sh
--- workload/create-workload.f
+++ workload/create-workload.f
@@ -4,10 +4,10 @@ define process name=readerP,instances=1 {
   thread name=readerT,instances=1 {
     flowop openfile name=openOP,filesetname=testF
     flowop readwholefile name=readOP,filesetname=testF
+    flowop writewholefile name=writeOP,filesetname=testF
     flowop closefile name=closeOP
     flowop statfile name=statOP,filesetname=testF
+    flowop listdir name=listOP,filesetname=testF
   }
 }
-
 run 60
-
